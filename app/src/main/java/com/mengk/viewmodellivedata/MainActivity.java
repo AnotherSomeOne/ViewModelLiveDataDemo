765e8ef0c64f4c7612fa76b95c52329cccaebbb4
MengK <jsmengk@itcom888.com>
2019-09-05 11:44:17 +0800

    task#0 在初始化时启动一个定时任务，每隔一秒通过postValue方法刷新一下数据。

--- app/src/main/java/com/mengk/viewmodellivedata/MainActivity.java
+++ app/src/main/java/com/mengk/viewmodellivedata/MainActivity.java
@@ -1,14 +1,46 @@
 package com.mengk.viewmodellivedata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.mengk.viewmodellivedata.viewmodel.LiveDataTimerViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LiveDataTimerViewModel mModel;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        mModel = ViewModelProviders.of(this).get(LiveDataTimerViewModel.class);
+
+        subscribe();
+    }
+
+
+    private void subscribe() {
+        mModel.getElapsedTime().observe(this, new Observer<Long>() {
+            @Override
+            public void onChanged(Long aLong) {
+                //Update UI
+                mTextView.setText("time = " + aLong);
+            }
+        });
+    }
+
+
+    private void initViews() {
+        mTextView = findViewById(R.id.text_view);
     }
 }
---
+++ app/src/main/java/com/mengk/viewmodellivedata/viewmodel/LiveDataTimerViewModel.java
@@ -0,0 +1,45 @@
+package com.mengk.viewmodellivedata.viewmodel;
+
+import android.os.SystemClock;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @author Mengk
+ * @date {2019/9/5}
+ * @description
+ */
+public class LiveDataTimerViewModel extends ViewModel {
+
+    private static final int ONE_SECOND = 1000;
+
+    private MutableLiveData<Long> mElapsedTime = new MutableLiveData<>();
+
+    private long mInitialTime;
+
+    /**
+     * 在初始化时启动一个定时任务，每隔一秒通过postValue方法刷新一下数据。
+     */
+    public LiveDataTimerViewModel() {
+        mInitialTime = SystemClock.elapsedRealtime();
+
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                long newValue = (SystemClock.elapsedRealtime() - mInitialTime) / 1000;
+                mElapsedTime.postValue(newValue);
+            }
+        },ONE_SECOND,ONE_SECOND);
+
+    }
+
+    public MutableLiveData<Long> getElapsedTime() {
+        return mElapsedTime;
+    }
+}
